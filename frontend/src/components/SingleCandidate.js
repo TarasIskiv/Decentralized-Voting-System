bfd04786a0243bd669b25fc093787b2683b3c04c
Taras Iskiv <taras.iskiv@gmail.com>
2024-08-04 15:55:55 +0200

    small styling fixes

--- frontend/src/components/SingleCandidate.js
+++ frontend/src/components/SingleCandidate.js
@@ -6,7 +6,7 @@ const SingleCandidate = ({candidate}) =>
                 <div className="col-2" style={{ paddingLeft: '0'}}>
                     <img src={candidate.image} alt='None' className="candidate-img" />
                 </div>
-                <div className="col-8 d-flex-justify-content-start candidate-info">
+                <div className="col-7 d-flex-justify-content-start candidate-info">
                     <p><span className="single-candidate-info">Name: </span> {candidate.name}</p>
                     <p><span className="single-candidate-info">Description: </span> {candidate.description}</p>
                     <p>
@@ -17,8 +17,10 @@ const SingleCandidate = ({candidate}) =>
                     </p>
                     <p><span className="single-candidate-info">Votes: </span> {candidate.votes}</p>
                 </div>
-                <div className="col d-flex align-items-center justify-content-center">
-                    <button className="btn btn-primary vote-btn">Vote</button>
+                <div className="col">
+                    <div className="d-flex justify-content-center align-items-center h-100">
+                        <button className="btn btn-primary vote-btn">Vote</button>
+                    </div>
                 </div>
             </div>
         </div>
